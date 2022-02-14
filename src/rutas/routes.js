7754b91e4f6c348a5335eab50fb5ca87b2a37b5a
Gixarde3 <antoniochavezmarco@gmail.com>
2022-02-13 22:48:37 -0600

    factura

--- src/rutas/routes.js
+++ src/rutas/routes.js
@@ -11,9 +11,28 @@ const {Router} = require('express');
 const { rawListeners } = require('process');
 const router = Router();
 const stripe = require("stripe")('sk_test_51KSDOZCSDiTjPacS84qjgU7KS1PzbqZIH9seCfwY38isK3BL1EcOs7fYr1YB0JOjqVrK467EUOP6D5NdqPojW7sE00rynd7pKc');
+const PDF = require('pdfkit-construct');
+
 
 app.use(express.static("public"));
 
+function formato(precioF){
+    precio = ""+precioF;
+    let precioFormato = "";
+    var cont = 0;
+    for(var j=precio.length-1; j >= 0; j--){
+        cont++;
+        precioFormato+=precio[j];
+        if(cont%3==0 && j != 0){
+            precioFormato+=',';
+        }
+    }
+    let formatoFinal="";
+    for(var j=precioFormato.length-1; j >= 0; j--){
+        formatoFinal+=precioFormato[j];
+    }
+    return formatoFinal;
+}
 
 router.post("/create-payment-intent", async (req, res) => {
   const { items } = req.body;
@@ -337,6 +356,10 @@ router.get('/venta', (req, res)=>{
             if(error){
                 throw error;
             }else{
+                req.session.descripcion = results[0].descripcion
+                req.session.precio = results[0].valor
+                req.session.vendidoA = results[0].a_nombre_de
+                req.session.concepto =  results[0].concepto
                 res.render('producto.html',{
                     venta: true,
                     id: req.session.idVenta,
@@ -360,17 +383,98 @@ router.get('/succes', async (req,res)=>{
         }else{
             res.render('index.html',{
                 id: req.session.idVenta,
+                save: true,
                 res: results[1],
                 alert: true,
-                alertTitle: "Producto pagado con éxito.",
-                alertMessage: "Redirigiendo a la página de inicio",
-                alertIcon: "success",
-                showConfirmButton: false,
-                time: 1500,
-                ruta: '/'
+                alertTitle: "Factura.",
+                alertMessage: "¿Desea guardar la factura?",
+                alertIcon: "question",
+                time: false,
+                showConfirmButton: true,
+                showDenyButton: true,
+                ruta: ''
             });
         }
     })
+})
+
+router.get('/generate-pdf', async(req, res)=>{
+    const doc = new PDF({
+        bufferPage: true,
+        size: 'letter',
+        margins: {top: 20, left: 10, right: 10, bottom: 20},
+        bufferPages: true,
+    })
+    
+    const venta = [
+        {
+            nVenta : req.session.idVenta,
+            descripcion: req.session.descripcion,
+            precio: "$"+formato(req.session.precio)+"Mxn",
+            vendidoA: req.session.vendidoA
+        }
+    ]
+    const stream = res.writeHead(200, {
+        'Content-Type':'application/pdf',
+        'Content-disposition':'attachment;filename=factura.pdf'
+    })
+    doc.setDocumentHeader({
+        height: '20',
+        width: '100'
+    }, () => {
+        doc.fill("#000")
+            .fontSize(20)
+            .text("Factura de venta de "+req.session.concepto, doc.header.x, doc.header.y);
+        doc.fontSize(14).text('                               ',{
+            width: 420,
+            align: 'center'
+        });
+        doc.fontSize(14).text('                               ',{
+            width: 420,
+            align: 'center'
+        });
+        doc.fontSize(14).text('                               ',{
+            width: 420,
+            align: 'center'
+        });
+        doc.fontSize(14).text('_____________________',{
+            width: 420,
+            align: 'center'
+        });
+        doc.text(req.session.vendidoA,{
+            width: 420,
+            align: 'center'
+        });
+        doc.fontSize(12).text('Firma de recibido',{
+            width: 420,
+            align: 'center'
+        });
+    });
+    doc.addTable(
+        [
+            {key: 'nVenta', label: 'Número de la venta', align: 'left'},
+            {key: 'descripcion', label: 'Descripción', align: 'left'},
+            {key: 'precio', label: 'Precio', align: 'right'},
+            {key: 'vendidoA', label: 'Vendido A: '},
+        ],
+        venta, {
+            border: null,
+            width: "fill_body",
+            striped: true,
+            stripedColors: ["#ffffff", "#fbfbfb"],
+            cellsPadding: 10,
+            marginLeft: 45,
+            marginRight: 45,
+            headAlign: 'center'
+        });
+        doc.render();
+        // this should be the last
+        // for this to work you need to set bufferPages to true in constructor options 
+        doc.setPageNumbers((p, c) => `Página ${p} de ${c}`, "bottom right");
+
+        doc.pipe(res);
+        doc.end();
+
 })
 
 router.get('/ventas', (req, res)=>{
